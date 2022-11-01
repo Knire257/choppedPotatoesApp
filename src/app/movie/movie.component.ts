f708daf877984dfd65305149361c58c257bfca58
Daniel Naranjo <daniel.naranjo1999@gmail.com>
2022-10-31 22:50:40 -0500

    Added review component, changed icon and added types

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import type {review, movie} from '../types/moviesTypes';
 
 @Component({
   selector: 'app-movie',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+  type='';
+  id ='';
+  url = '';
+  movies: any;
+  movieSelected: any;
+
+  constructor(private route:ActivatedRoute, private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.type = this.route.snapshot.params['type'];
+    this.id = this.route.snapshot.params['id'];
+    this.url = 'http://localhost:4200/assets/data/';
+    this.setUrl();
+    this.getMovie();
+  }
+
+  setUrl() {
+    switch (this.type) {
+      case 'trending':
+        this.url += 'trending-movies.json';
+        break;
+      case 'popular':
+        this.url += 'popular-movies.json';
+        break;
+      case 'theatre':
+        this.url += 'theatre-movies.json';
+        break;
+    }
+  }
+
+  getMovie() {
+    this.http.get(this.url).subscribe((movies) => {
+      this.movies = movies;
+      this.movieSelected = this.movies.find((movie:movie) => +movie.id === +this.id);
+    });
   }
 
 }
